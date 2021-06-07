05f6eae7735fef20678ac7121f35d185e6bd61c1
Gabriel Liwerant <gabriel@abrightconcept.com>
2021-06-07 02:48:08 -0400

    Allow editing of todo items

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,46 @@
 import React, { useState } from 'react';
 
+const makeNewTodo = (id, text) => ({
+  id,
+  text,
+  isEditActive: false
+});
+
 const App = () => {
   const [todos, setTodos] = useState([]);
 
   const addTodo = () => {
     const newTodos = todos.map(todo => todo);
+    const id = Math.floor(Math.random() * 1000000);
 
-    newTodos.push('New todo');
+    newTodos.push(makeNewTodo(id, 'New todo'));
+
+    setTodos(newTodos);
+  };
+
+  const editTodo = id => () => {
+    const newTodos = todos.map(todo => {
+      if (todo.id === id) todo.isEditActive = true;
+      return todo;
+    });
+
+    setTodos(newTodos);
+  };
+
+  const onChange = id => e => {
+    const newTodos = todos.map(todo => {
+      if (todo.id === id) todo.text = e.target.value;
+      return todo;
+    });
+
+    setTodos(newTodos);
+  };
+
+  const onSave = id => () => {
+    const newTodos = todos.map(todo => {
+      if (todo.id === id) todo.isEditActive = false;
+      return todo;
+    });
 
     setTodos(newTodos);
   };
@@ -15,7 +49,25 @@ const App = () => {
     <>
       <div>Todo List</div>
       <ul>
-        {todos.map((todo, i) => <li key={i} id={i}>{todo}</li>)}
+        {todos.map(todo => (
+          <li key={todo.id} id={todo.id}>
+            <span
+              onClick={editTodo(todo.id)}
+              style={{ display: todo.isEditActive ? 'none' : 'inline' }}
+            >
+              {todo.text}
+              {console.log(todo)}
+            </span>
+            <div style={{ display: todo.isEditActive ? 'block' : 'none' }}>
+              <input
+                onChange={onChange(todo.id)}
+                type='text'
+                value={todo.text}
+              />
+              <button onClick={onSave(todo.id)}>Save</button>
+            </div>
+          </li>
+        ))}
       </ul>
       <button onClick={addTodo}>Add Todo</button>
     </>
