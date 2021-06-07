a188dff2035de83d8b5b3e26b8a5b851d352aae0
Gabriel Liwerant <gabriel@abrightconcept.com>
2021-06-07 02:03:42 -0400

    Add initial todo list and add button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const App = () => (
-  <h1>Hello World!</h1>
-);
+const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = () => {
+    const newTodos = todos.map(todo => todo);
+
+    newTodos.push('New todo');
+
+    setTodos(newTodos);
+  };
+
+  return (
+    <>
+      <div>Todo List</div>
+      <ul>
+        {todos.map((todo, i) => <li key={i} id={i}>{todo}</li>)}
+      </ul>
+      <button onClick={addTodo}>Add Todo</button>
+    </>
+  )
+};
 
 export default App;
