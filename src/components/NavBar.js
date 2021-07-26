f29f2076690ea458cd5cc851a4c9b98c8089651c
Gabriel Liwerant <gabriel@abrightconcept.com>
2021-07-26 00:09:06 -0400

    Add ability to edit project name

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,10 +5,11 @@ import { createUseStyles } from 'react-jss';
 import AddTwoToneIcon from '@material-ui/icons/AddTwoTone';
 
 import { Z_INDEX } from '../jss/constants';
-import { todosSlice, listsSlice } from '../reducers';
+import { todosSlice, listsSlice, projectsSlice } from '../reducers';
 import { getProjectsSort, getProjectsItems } from '../selectors';
 import { makeId } from '../utils';
 import Button from './Button';
+import NameInputEdit from './NameInputEdit';
 
 const useStyles = createUseStyles({
   container: {
@@ -24,23 +25,51 @@ const useStyles = createUseStyles({
     alignItems: 'center',
     justifyContent: 'space-between',
     padding: '15px 20px'
+  },
+  text: {
+    fontSize: '18px'
   }
 });
 
-const NavBar = ({ addSort, add, projectsSort, projectsItems }) => {
+const NavBar = ({
+  addSort,
+  add,
+  edit,
+  save,
+  cancel,
+  change,
+  projectsSort,
+  projectsItems
+}) => {
   const classes = useStyles();
   const onClick = () => {
     const id = makeId();
     addSort({ id });
     add({ id });
   };
+  const onChange = id => e => change({ id, draft: e.target.value });
 
   return (
     <header className={classes.container}>
       <nav className={classes.inner}>
         <ul>
           {projectsSort['1'].map(projectId => (
-            <li key={projectId}>{projectsItems[projectId].text.final}</li>
+            <li key={projectId}>
+              <NameInputEdit
+                onClickEdit={edit({ id: projectId })}
+                onChangeEdit={onChange(projectId)}
+                onClickSave={save({
+                  id: projectId,
+                  draft: projectsItems[projectId].text.draft
+                })}
+                onClickCancel={cancel({ id: projectId })}
+                textFinal={projectsItems[projectId].text.final}
+                textDraft={projectsItems[projectId].text.draft}
+                isEditActive={projectsItems[projectId].isEditActive}
+                isComplete={false}
+                myClassNames={{ text: classes.text }}
+              />
+            </li>
           ))}
         </ul>
         <ul>
@@ -58,6 +87,10 @@ const NavBar = ({ addSort, add, projectsSort, projectsItems }) => {
 NavBar.propTypes = {
   addSort: PropTypes.func.isRequired,
   add: PropTypes.func.isRequired,
+  edit: PropTypes.func.isRequired,
+  save: PropTypes.func.isRequired,
+  cancel: PropTypes.func.isRequired,
+  change: PropTypes.func.isRequired,
   projectsSort: PropTypes.object.isRequired,
   projectsItems: PropTypes.object.isRequired
 };
@@ -69,7 +102,11 @@ const mapStateToProps = () => ({
 
 const mapDispatchToProps = dispatch => ({
   addSort: listId => dispatch(todosSlice.actions.addSort(listId)),
-  add: (listId, projectId) => dispatch(listsSlice.actions.add(listId, projectId))
+  add: (listId, projectId) => dispatch(listsSlice.actions.add(listId, projectId)),
+  edit: id => () => dispatch(projectsSlice.actions.edit(id)),
+  save: (id, draft) => () => dispatch(projectsSlice.actions.save(id, draft)),
+  cancel: id => () => dispatch(projectsSlice.actions.cancel(id)),
+  change: id => dispatch(projectsSlice.actions.change(id))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
